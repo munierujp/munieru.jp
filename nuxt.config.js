3c46c885c6cd9f65573ad2958ccc8e4cba1a6fab
Munieru <20086673+munierujp@users.noreply.github.com>
2019-01-19 01:39:22 +0900

    Update nuxt.config.js
    Add nuxt-i18n module

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,7 @@
 const Stylelint = require('stylelint-webpack-plugin')
-const pkg = require('./package')
+
+const i18n = require('./nuxt-i18n.config')
+const lang = require(`./${i18n.langDir}${i18n.defaultLocale}`)
 
 module.exports = {
   mode: 'spa',
@@ -8,11 +10,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: lang.APP_NAME,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: lang.APP_DESCRIPTION }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
@@ -43,6 +45,7 @@ module.exports = {
   ** Nuxt.js modules
   */
   modules: [
+    ['nuxt-i18n', i18n]
   ],
 
   /*
