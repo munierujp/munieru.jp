4ea6ab0fe399cacb9f3ff21d3cb6ac2251c1adc9
Munieru <20086673+munierujp@users.noreply.github.com>
2019-02-06 23:53:37 +0900

    Add AppHeader2 component

---
+++ components/AppHeader2.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2 class="app-header-2"><slot/></h2>
+</template>
+
+<style lang="scss" scoped>
+$relative-max-width: 600px;
+$ratio: 0.1;
+$relative-font-size: 100vw * $ratio;
+$absolute-font-size: $relative-max-width * $ratio;
+
+.app-header-2 {
+  overflow-wrap: break-word;
+  font-size: $absolute-font-size;
+}
+
+@media (max-width: $relative-max-width) {
+  .app-header-2 {
+    font-size: $relative-font-size;
+  }
+}
+</style>
