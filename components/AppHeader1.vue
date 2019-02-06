7f62f1c30f661c10fec21e27ab6c1e00d0f24cff
Munieru <20086673+munierujp@users.noreply.github.com>
2019-02-06 23:53:26 +0900

    Add AppHeader1 component

---
+++ components/AppHeader1.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="app-header-1"><slot/></h1>
+</template>
+
+<style lang="scss" scoped>
+$relative-max-width: 600px;
+$ratio: 0.2;
+$relative-font-size: 100vw * $ratio;
+$absolute-font-size: $relative-max-width * $ratio;
+
+.app-header-1 {
+  overflow-wrap: break-word;
+  font-size: $absolute-font-size;
+}
+
+@media (max-width: $relative-max-width) {
+  .app-header-1 {
+    font-size: $relative-font-size;
+  }
+}
+</style>
