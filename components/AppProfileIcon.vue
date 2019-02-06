08d29f3394b897bcf95ed140134b8899337b6123
Munieru <20086673+munierujp@users.noreply.github.com>
2019-02-07 00:03:21 +0900

    Add AppProfileIcon component

---
+++ components/AppProfileIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <img
+    :src="url"
+    class="app-profile-icon">
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$relative-max-width: 600px;
+$absolute-size: 300px;
+$relative-size: 100vw * ($absolute-size / $relative-max-width);
+
+.app-profile-icon {
+  width: $absolute-size;
+  height: $absolute-size;
+  border: {
+    radius: 50%;
+  }
+}
+
+@media (max-width: $relative-max-width) {
+  .app-profile-icon {
+    width: $relative-size;
+    height: $relative-size;
+  }
+}
+</style>
