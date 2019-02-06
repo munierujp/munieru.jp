e4a822b377c63a26c40b62a61899e36be75b3595
Munieru <20086673+munierujp@users.noreply.github.com>
2019-02-07 00:03:55 +0900

    Add AppSkillButton component

---
+++ components/AppSkillButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-chip
+    outline
+    color="white">{{ name }}</v-chip>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
