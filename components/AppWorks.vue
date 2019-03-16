69289bdce3e73661100634c9bce7d30db7b48f41
Munieru <20086673+munierujp@users.noreply.github.com>
2019-03-16 21:50:46 +0900

    Add AppWorks component

---
+++ components/AppWorks.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout
+    justify-space-around
+    wrap>
+    <v-flex
+      v-for="work in works"
+      :key="work.title"
+      xs12
+      sm6>
+      <app-work-card v-bind="work" />
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import works from '~/modules/works.json'
+import AppWorkCard from '~/components/AppWorkCard'
+
+export default {
+  components: {
+    AppWorkCard
+  },
+  data: () => ({
+    works
+  })
+}
+</script>
