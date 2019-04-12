1d4f788969b97c342b7ac5b75d8b51452655eced
Munieru <20086673+munierujp@users.noreply.github.com>
2019-04-13 03:22:17 +0900

    Update index page (#26)
    * Add AppText component
    
    * Update index page
    
    * Add border

---
+++ components/AppText.vue
@@ -0,0 +1,26 @@
+<template>
+  <span :style="style"><slot/></span>
+</template>
+
+<script>
+export default {
+  props: {
+    fontSize: {
+      type: String,
+      default: ''
+    },
+    fontWeight: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    style () {
+      return {
+        'font-size': this.fontSize,
+        'font-weight': this.fontWeight
+      }
+    }
+  }
+}
+</script>
