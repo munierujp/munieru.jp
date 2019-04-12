04615e0bd7f457a9b5ee7108afe97f4c563377c8
Munieru <20086673+munierujp@users.noreply.github.com>
2019-04-12 23:51:13 +0900

    Add AppPayPayButton component

---
+++ components/AppPayPayButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <app-button
+      v-bind="link"
+      @click.stop="open" />
+    <v-dialog
+      v-model="showDialog"
+      width="320px">
+      <v-card>
+        <v-card-title class="headline">
+          {{ $t('DONATE_WITH_PAYPAY_TITLE') }}
+        </v-card-title>
+        <v-card-text>
+          <v-img src="paypay.png" />
+        </v-card-text>
+        <v-card-text>
+          {{ $t('DONATE_WITH_PAYPAY_DESCRIPTION') }}
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn
+            flat
+            @click="close">
+            {{ $t('CLOSE') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import AppButton from '~/components/AppButton'
+
+export default {
+  components: {
+    AppButton
+  },
+  data: () => ({
+    showDialog: false
+  }),
+  computed: {
+    link () {
+      return {
+        title: this.$t('PAYPAY'),
+        label: this.$t('PAYPAY'),
+        image: 'https://image.paypay.ne.jp/favicon.ico',
+        color: '#ff0033',
+        dark: true
+      }
+    }
+  },
+  methods: {
+    open () {
+      this.showDialog = true
+    },
+    close () {
+      this.showDialog = false
+    }
+  }
+}
+</script>
