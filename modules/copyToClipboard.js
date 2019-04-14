e66e24eb85b7d51b28dae1c07e85b4cde84a0790
Munieru <20086673+munierujp@users.noreply.github.com>
2019-04-15 01:22:48 +0900

    Fix copyToClipboard function

--- modules/copyToClipboard.js
+++ modules/copyToClipboard.js
@@ -1,15 +1,32 @@
 export default function (value) {
-  const input = document.createElement('input')
-  hideElementSpuriously(input)
+  const input = createInputElement()
   document.body.appendChild(input)
   input.value = value
-  input.select()
+  selectText(input)
   const success = document.execCommand('copy')
   document.body.removeChild(input)
   return success
 }
 
+function createInputElement () {
+  const input = document.createElement('input')
+  input.setAttribute('readonly', 'readonly') // for iOS/Android
+  input.style.fontSize = '16px' // for iOS
+  hideElementSpuriously(input)
+  return input
+}
+
 function hideElementSpuriously (element) {
   element.style.position = 'fixed'
   element.style.zIndex = '-1'
+}
+
+function selectText (element) {
+  element.select()
+  const range = document.createRange()
+  range.selectNodeContents(element)
+  const selection = window.getSelection()
+  selection.removeAllRanges()
+  selection.addRange(range)
+  element.setSelectionRange(0, 999999)
 }
