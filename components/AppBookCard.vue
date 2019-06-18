c2292912b0894c5b93721547ea3c7166cc069ba0
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-19 00:19:49 +0900

    Add Books (#52)
    * Add AppBookCard component
    
    * Add books.js
    
    * Add AppBooks component
    
    * Add HEADER_BOOKS
    
    * Add Books

---
+++ components/AppBookCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card
+    :href="url"
+    target="_blank"
+    width="320px"
+    max-width="100%"
+  >
+    <v-img :src="image" />
+    <v-card-title primary-title>
+      <div>
+        <div class="grey--text">
+          <span>{{ date }}</span>
+        </div>
+        <div class="headline">
+          <span>{{ title }}</span>
+        </div>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ components/AppBooks.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-layout
+    justify-space-around
+    wrap
+    class="app-books"
+  >
+    <v-flex
+      v-for="book in books"
+      :key="book.key"
+      class="app-book"
+    >
+      <app-book-card v-bind="book" />
+    </v-flex>
+    <!-- dummy components for layout (align left) -->
+    <v-flex
+      v-for="i in books.length"
+      :key="i"
+      class="app-book app-book-dummy"
+    >
+      <div />
+    </v-flex>
+    <!-- /dummy components for layout (align left) -->
+  </v-layout>
+</template>
+
+<script>
+import books from '~/modules/books'
+import AppBookCard from '~/components/AppBookCard'
+
+export default {
+  components: {
+    AppBookCard
+  },
+  data: () => ({
+    books
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+$book-card-width: 320px;
+
+.app-book {
+  flex-grow: 0;
+}
+
+.app-book-dummy {
+  height: 0;
+  padding-top: 0 !important;
+  padding-bottom: 0 !important;
+
+  div {
+    width: $book-card-width;
+  }
+}
+</style>
