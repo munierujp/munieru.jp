4674f5007f077088c93c1531da685a703da73191
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-19 00:08:06 +0900

    Add books.js

---
+++ modules/books.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    key: 'tech_do_book_vol_1',
+    title: 'Tech Do Book Vol.1',
+    date: '2019-04-14',
+    url: 'https://booth.pm/ja/items/1318348',
+    image: 'https://booth.pximg.net/c6fe2cd0-596d-4d27-b84c-4d516f651dee/i/1318348/24b02161-a908-43c6-bf5c-1263e848de06_base_resized.jpg'
+  }
+]
