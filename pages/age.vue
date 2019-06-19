9fa09091b64ffd94a2a304384a9546eba14c98b2
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-19 09:57:57 +0900

    Add age page

---
+++ pages/age.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fill-height>
+    <v-layout align-center>
+      <v-flex text-xs-center>
+        <app-age-text />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import AppAgeText from '~/components/AppAgeText'
+
+export default {
+  layout: 'fullscreen',
+  components: {
+    AppAgeText
+  }
+}
+</script>
