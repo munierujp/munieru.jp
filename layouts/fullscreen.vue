b0ac33fdf603cb21debdc8a59727319b63848095
Munieru <20086673+munierujp@users.noreply.github.com>
2019-06-19 10:00:32 +0900

    Add age page (#54)
    * Add fullscreen.vue
    
    * Add get-age package
    
    `npm i -D get-age`
    
    * Add AGE
    
    * Add AppAgeText component
    
    * Add age page

---
+++ layouts/fullscreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app id="app">
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="scss">
+#app {
+  color: #293030;
+  background-color: #f9fafb;
+  font-family: Roboto, "Noto Sans JP", sans-serif;
+}
+</style>
