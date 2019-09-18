2322dbbc44a4d30476bfb9b191b05735ba30bd11
Munieru <20086673+munierujp@users.noreply.github.com>
2019-09-18 12:13:37 +0900

    Fix layout

--- pages/age.vue
+++ pages/age.vue
@@ -1,11 +1,9 @@
 <template>
-  <v-container fill-height>
-    <v-layout align-center>
-      <v-flex text-xs-center>
-        <app-age-text />
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout align-center>
+    <v-flex text-xs-center>
+      <app-age-text />
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,80 @@
 <template>
-  <v-container grid-list-xl>
-    <v-layout
-      column
-      align-center
-    >
-      <v-flex>
-        <app-header-1>{{ $t('APP_NAME') }}</app-header-1>
-      </v-flex>
-      <v-flex>
-        <app-profile-icon url="icon.png" />
-      </v-flex>
-      <v-flex text-xs-center>
-        <app-text
-          font-size="24px"
-          font-weight="bold"
-        >
-          {{ $t('NAME') }}
-        </app-text>
-      </v-flex>
-      <v-flex text-xs-center>
-        <app-text
-          font-size="16px"
-          v-html="$t('BIO')"
-        />
-      </v-flex>
-      <v-flex>
-        <app-header-2>{{ $t('HEADER_LINKS') }}</app-header-2>
-      </v-flex>
-      <v-flex text-xs-center>
-        <app-link-button
-          v-for="link in links"
-          :key="link.title"
-          v-bind="link"
-        />
-      </v-flex>
-      <v-flex>
-        <app-header-2>{{ $t('HEADER_SKILLS') }}</app-header-2>
-      </v-flex>
-      <v-flex text-xs-center>
-        <app-skill-button
-          v-for="skill in skills"
-          :key="skill.name"
-          v-bind="skill"
-        />
-      </v-flex>
-      <v-flex>
-        <app-header-2>{{ $t('HEADER_WORKS') }}</app-header-2>
-      </v-flex>
-      <v-flex>
-        <app-works />
-      </v-flex>
-      <v-flex>
-        <app-header-2>{{ $t('HEADER_BOOKS') }}</app-header-2>
-      </v-flex>
-      <v-flex>
-        <app-books />
-      </v-flex>
-      <v-flex>
-        <app-header-2>{{ $t('HEADER_DONATE') }}</app-header-2>
-      </v-flex>
-      <v-flex text-xs-center>
-        <v-layout
-          justify-space-around
-          wrap
-        >
-          <v-flex>
-            <app-pay-pay-button />
-          </v-flex>
-          <v-flex>
-            <app-wish-list-button />
-          </v-flex>
-          <v-flex>
-            <app-giftee-button />
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout
+    column
+    align-center
+  >
+    <v-flex>
+      <app-header-1>{{ $t('APP_NAME') }}</app-header-1>
+    </v-flex>
+    <v-flex>
+      <app-profile-icon url="icon.png" />
+    </v-flex>
+    <v-flex text-xs-center>
+      <app-text
+        font-size="24px"
+        font-weight="bold"
+      >
+        {{ $t('NAME') }}
+      </app-text>
+    </v-flex>
+    <v-flex text-xs-center>
+      <app-text
+        font-size="16px"
+        v-html="$t('BIO')"
+      />
+    </v-flex>
+    <v-flex>
+      <app-header-2>{{ $t('HEADER_LINKS') }}</app-header-2>
+    </v-flex>
+    <v-flex text-xs-center>
+      <app-link-button
+        v-for="link in links"
+        :key="link.title"
+        v-bind="link"
+      />
+    </v-flex>
+    <v-flex>
+      <app-header-2>{{ $t('HEADER_SKILLS') }}</app-header-2>
+    </v-flex>
+    <v-flex text-xs-center>
+      <app-skill-button
+        v-for="skill in skills"
+        :key="skill.name"
+        v-bind="skill"
+      />
+    </v-flex>
+    <v-flex>
+      <app-header-2>{{ $t('HEADER_WORKS') }}</app-header-2>
+    </v-flex>
+    <v-flex>
+      <app-works />
+    </v-flex>
+    <v-flex>
+      <app-header-2>{{ $t('HEADER_BOOKS') }}</app-header-2>
+    </v-flex>
+    <v-flex>
+      <app-books />
+    </v-flex>
+    <v-flex>
+      <app-header-2>{{ $t('HEADER_DONATE') }}</app-header-2>
+    </v-flex>
+    <v-flex text-xs-center>
+      <v-layout
+        justify-space-around
+        wrap
+      >
+        <v-flex>
+          <app-pay-pay-button />
+        </v-flex>
+        <v-flex>
+          <app-wish-list-button />
+        </v-flex>
+        <v-flex>
+          <app-giftee-button />
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
