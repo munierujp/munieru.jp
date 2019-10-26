40afe402e49921c51cd686b8dccb16303b2184dd
Munieru <20086673+munierujp@users.noreply.github.com>
2019-10-27 00:02:32 +0900

    Add PayPal to Donate section (#80)
    * Add PAYPAL
    
    * Add PAYPAL_URL
    
    * Add AppPayPalButton.vue
    
    * Add AppPayPalButton

---
+++ components/matters/AppPayPalButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <app-link-button v-bind="link" />
+</template>
+
+<script>
+import AppLinkButton from '~/components/elements/AppLinkButton'
+
+const { PAYPAL_URL } = process.env.config
+
+export default {
+  components: {
+    AppLinkButton
+  },
+  computed: {
+    link () {
+      return {
+        title: this.$t('PAYPAL'),
+        label: this.$t('PAYPAL'),
+        url: PAYPAL_URL,
+        icon: 'fab fa-paypal',
+        color: '#003087',
+        dark: true
+      }
+    }
+  }
+}
+</script>
